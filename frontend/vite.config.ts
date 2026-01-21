648f4227ab031d1ffb05489057fe1c0461dffa39
usrlocal <usrlocal@GA213-FE06.ad.univ-angers.fr>
2026-01-21 10:32:35 +0100

    add the import ...

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
